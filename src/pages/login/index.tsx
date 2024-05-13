fe1efc6c75c05e1e26871a2f0788ddc44879d821
Ian Koech <ian.koech@fin.africa>
2024-05-13 12:20:00 +0300

    Created login page

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,9 +1,9 @@
 import React, { useState } from "react";
 import { FiEye, FiEyeOff } from "react-icons/fi";
 import logo from "../../../public/assets/FIN Welcome Page Assets/Fin Logo (1).png";
-import regImage from "../../../public/assets/4351531 1.png";
+import loginImage from "../../../public/assets/6461498 1.png";
 
-const Register = () => {
+const Login = () => {
   const [showPassword, setShowPassword] = useState(false);
   const [showConfirmPassword, setShowConfirmPassword] = useState(false);
 
@@ -23,19 +23,7 @@ const Register = () => {
       <div className="bg-white p-8 rounded-lg shadow-lg flex flex-col md:flex-row max-w-4xl w-full">
         <div className="md:w-1/2 md:pr-4">
           <h2 className="text-2xl font-normal mb-2 text-black">Welcome !</h2>
-          <h1 className="text-black text-3xl font-semibold mb-5">Register</h1>
-          <div className="mb-6 relative">
-            <label htmlFor="email" className="block text-gray-700 mb-1">
-              Email
-            </label>
-            <input
-              type="email"
-              id="email"
-              name="email"
-              placeholder="Enter your email"
-              className="mt-1 block w-full rounded border border-gray-300 px-3 py-2 focus:outline-none focus:border-indigo-300"
-            />
-          </div>
+          <h1 className="text-black text-3xl font-semibold mb-5">Sign In</h1>
           <div className="mb-6 relative">
             <label htmlFor="username" className="block text-gray-700 mb-1">
               Username
@@ -69,48 +57,33 @@ const Register = () => {
               </button>
             </div>
           </div>
-          <div className="mb-6 relative">
-            <label
-              htmlFor="confirm-password"
-              className="block text-gray-700 mb-1"
-            >
-              Confirm Password
-            </label>
-            <div className="relative">
-              <input
-                type={showConfirmPassword ? "text" : "password"}
-                id="confirm-password"
-                name="confirm-password"
-                placeholder="Confirm your Password"
-                className="mt-1 block w-full rounded border border-gray-300 px-3 py-2 focus:outline-none focus:border-indigo-300"
-              />
-              <button
-                type="button"
-                onClick={toggleConfirmPasswordVisibility}
-                className="absolute inset-y-0 right-0 px-3 py-2 top-1/2 transform -translate-y-1/2 focus:outline-none"
-              >
-                {showConfirmPassword ? <FiEyeOff /> : <FiEye />}
-              </button>
-            </div>
-          </div>
-          <button className="w-full bg-green-300 text-white py-2 rounded-md hover:bg-green-500 transition duration-300">
-            Register
+
+          <button className="w-full bg-green-300 text-white py-2 rounded-md hover:bg-green-600 transition duration-300">
+            Login
           </button>
+          <div className="flex justify-between items-center mt-4 text-sm">
+            <div className="flex items-center">
+              <input type="checkbox" id="remember_me" className="mr-2" />
+              <label htmlFor="remember_me">Remember me</label>
+            </div>
+            <span className="text-gray-600">Forgot Password ?</span>
+          </div>
+
           <br />
           <br />
           <div className="justify-center align-middle text-black font-thin pl-12">
             <p>
-              Already have an Account ?{" "}
-              <span className="font-semibold">Sign In</span>
+              Don't have an Account ?{" "}
+              <span className="font-semibold">Register</span>
             </p>
           </div>
         </div>
 
         <div className="md:w-1/2 md:pl-4 mt-4 md:mt-0 pt-20">
           <img
-            src={regImage.src}
+            src={loginImage.src}
             alt="Image"
-            className="w-full h-auto md:w-200 md:h-auto rounded-lg"
+            className="w-full h-auto md:w-120 md:h-120 rounded-lg"
           />
         </div>
       </div>
@@ -118,4 +91,4 @@ const Register = () => {
   );
 };
 
-export default Register;
+export default Login;
