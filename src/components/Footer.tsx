21faa01b33e3b7bd2bf5cd8910de9384d1813a3a
Ian Koech <ian.koech@fin.africa>
2024-05-15 15:35:45 +0300

    Corrected footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -20,7 +20,32 @@ const Footer = () => {
           </p>
         </div>
 
-        {/* Other Footer Content */}
+        <div className="flex flex-col items-center mx-4 mb-4 sm:mb-0">
+          <h2 className="font-medium mb-2">Company</h2>
+          <p className="mb-1 font-extralight">About us</p>
+          <p className="mb-1 font-extralight">Blog</p>
+          <p className="mb-1 font-extralight">Careers</p>
+          <p className="mb-1 font-extralight">Community</p>
+        </div>
+
+        <div className="flex flex-col items-center mx-4 mb-4 sm:mb-0">
+          <h2 className="font-normal mb-2">Join us</h2>
+          <p className="mb-1 font-extralight">Merchants</p>
+          <p className="mb-1 font-extralight">Partners</p>
+        </div>
+
+        <div className="flex flex-col items-center mx-4 mb-4 sm:mb-0">
+          <h2 className="font-normal mb-2">Help</h2>
+          <p className="mb-1 font-extralight">Contact</p>
+          <p className="mb-1 font-extralight">FAQs</p>
+        </div>
+
+        <div className="flex flex-col items-center mx-4 mb-4 sm:mb-0">
+          <h2 className="font-normal mb-2">Legal</h2>
+          <p className="mb-1 font-extralight">Terms & Conditions</p>
+          <p className="mb-1 font-extralight">Privacy Policy</p>
+          <p className="mb-1 font-extralight">PAIA Manual</p>
+        </div>
 
         <div className="flex flex-col items-center mx-4 mb-4 sm:mb-0">
           <h2 className="font-extralight mb-2 text-4xl">
