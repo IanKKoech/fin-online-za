e629bfbbde7e686e778ac90257632bcf578593a7
Ian Koech <ian.koech@fin.africa>
2024-05-16 10:22:45 +0300

    commented out services & utils

--- src/services/supabase.ts
+++ src/services/supabase.ts
@@ -1,204 +1,204 @@
-import { SupabaseClient, PostgrestResponse } from "@supabase/supabase-js";
-import { Database as SupabaseDB } from "@/types/supabase";
-type Orders = SupabaseDB["public"]["Tables"]["orders"]["Row"];
+// import { SupabaseClient, PostgrestResponse } from "@supabase/supabase-js";
+// import { Database as SupabaseDB } from "@/types/supabase";
+// type Orders = SupabaseDB["public"]["Tables"]["orders"]["Row"];
 
-type FilterCriteria = Record<string, any>;
+// type FilterCriteria = Record<string, any>;
 
-interface Order {
-  field: string;
-  ascending: boolean;
-}
+// interface Order {
+//   field: string;
+//   ascending: boolean;
+// }
 
-export interface SupabaseError {
-  message: string;
-  status: number;
-  errors?: string[];
-}
+// export interface SupabaseError {
+//   message: string;
+//   status: number;
+//   errors?: string[];
+// }
 
-export const supabaseEdge = (supabase: SupabaseClient) => {
-  const callEdgeFunction = async <T>(
-    functionName: string,
-    payload?: object,
-    orderId?: string,
-    product?: string
-  ): Promise<T> => {
-    try {
-      let response: { data: any; error: any } = undefined;
-      if (orderId) {
-        response = await supabase
-          .from("orders")
-          .select("*")
-          .eq("id", orderId)
-          .single()
-          .then(async (order) => {
-            if (order.data) {
-              return await supabase.functions.invoke(functionName, {
-                ...payload,
-                headers: {
-                  "organisation-id": order.data["organisation_id"],
-                  "merchant-id": order.data["linked_merchant"],
-                },
-              });
-            }
-          });
-      } else {
-        response = await supabase
-          .from("organisation_products")
-          .select(`organisation(organisation_id)`)
-          .eq("id", product)
-          .single()
-          .then(async (response) => {
-            console.log(response.data.organisation["organisation_id"]);
-            return await supabase.functions.invoke(functionName, {
-              ...payload,
-              headers: {
-                "organisation-id":
-                  response.data.organisation["organisation_id"],
-              },
-            });
-          });
-      }
+// export const supabaseEdge = (supabase: SupabaseClient) => {
+//   const callEdgeFunction = async <T>(
+//     functionName: string,
+//     payload?: object,
+//     orderId?: string,
+//     product?: string
+//   ): Promise<T> => {
+//     try {
+//       let response: { data: any; error: any } = undefined;
+//       if (orderId) {
+//         response = await supabase
+//           .from("orders")
+//           .select("*")
+//           .eq("id", orderId)
+//           .single()
+//           .then(async (order) => {
+//             if (order.data) {
+//               return await supabase.functions.invoke(functionName, {
+//                 ...payload,
+//                 headers: {
+//                   "organisation-id": order.data["organisation_id"],
+//                   "merchant-id": order.data["linked_merchant"],
+//                 },
+//               });
+//             }
+//           });
+//       } else {
+//         response = await supabase
+//           .from("organisation_products")
+//           .select(`organisation(organisation_id)`)
+//           .eq("id", product)
+//           .single()
+//           .then(async (response) => {
+//             console.log(response.data.organisation["organisation_id"]);
+//             return await supabase.functions.invoke(functionName, {
+//               ...payload,
+//               headers: {
+//                 "organisation-id":
+//                   response.data.organisation["organisation_id"],
+//               },
+//             });
+//           });
+//       }
 
-      if (response.error) {
-        console.log(response.error);
-        const supabaseError: SupabaseError = {
-          message:
-            JSON.stringify(response) ||
-            "An error occurred while calling the edge function.",
-          status: response.error.status || 500,
-          errors: response.error.errors || undefined,
-        };
-        throw supabaseError;
-      }
-      return response.data;
-    } catch (error) {
-      console.log(error);
-      const supabaseError: SupabaseError = {
-        message:
-          error.message || "An error occurred while calling the edge function.",
-        status: error.status || 500,
-      };
-      throw supabaseError;
-    }
-  };
+//       if (response.error) {
+//         console.log(response.error);
+//         const supabaseError: SupabaseError = {
+//           message:
+//             JSON.stringify(response) ||
+//             "An error occurred while calling the edge function.",
+//           status: response.error.status || 500,
+//           errors: response.error.errors || undefined,
+//         };
+//         throw supabaseError;
+//       }
+//       return response.data;
+//     } catch (error) {
+//       console.log(error);
+//       const supabaseError: SupabaseError = {
+//         message:
+//           error.message || "An error occurred while calling the edge function.",
+//         status: error.status || 500,
+//       };
+//       throw supabaseError;
+//     }
+//   };
 
-  const selectByFieldPostgrest = async <T>(
-    table: string,
-    field: string,
-    key: string
-  ): Promise<T> => {
-    try {
-      const response: PostgrestResponse<T> = await supabase
-        .from(table)
-        .select()
-        .eq(field, key);
-      if (response.error) {
-        const supabaseError: SupabaseError = {
-          message:
-            response.error.message ||
-            "An error occurred while calling the Postgrest endpoint.",
-          status: response.status || 500,
-          errors: getErrorDetails(response.error),
-        };
-        throw supabaseError;
-      }
-      if (Array.isArray(response.data) && response.data.length === 1) {
-        return response.data[0];
-      }
+//   const selectByFieldPostgrest = async <T>(
+//     table: string,
+//     field: string,
+//     key: string
+//   ): Promise<T> => {
+//     try {
+//       const response: PostgrestResponse<T> = await supabase
+//         .from(table)
+//         .select()
+//         .eq(field, key);
+//       if (response.error) {
+//         const supabaseError: SupabaseError = {
+//           message:
+//             response.error.message ||
+//             "An error occurred while calling the Postgrest endpoint.",
+//           status: response.status || 500,
+//           errors: getErrorDetails(response.error),
+//         };
+//         throw supabaseError;
+//       }
+//       if (Array.isArray(response.data) && response.data.length === 1) {
+//         return response.data[0];
+//       }
 
-      return response.data as T;
-    } catch (error) {
-      const supabaseError: SupabaseError = {
-        message:
-          error.message ||
-          "An error occurred while calling the Postgrest endpoint.",
-        status: error.status || 500,
-      };
-      throw supabaseError;
-    }
-  };
+//       return response.data as T;
+//     } catch (error) {
+//       const supabaseError: SupabaseError = {
+//         message:
+//           error.message ||
+//           "An error occurred while calling the Postgrest endpoint.",
+//         status: error.status || 500,
+//       };
+//       throw supabaseError;
+//     }
+//   };
 
-  const updateFieldPostgrest = async <T>(
-    table: string,
-    key: string,
-    field: string,
-    payload?: object
-  ): Promise<T> => {
-    try {
-      const response: PostgrestResponse<T> = await supabase
-        .from(table)
-        .update(payload)
-        .eq(field, key)
-        .select()
-        .single();
-      if (response.error) {
-        const supabaseError: SupabaseError = {
-          message:
-            response.error.message ||
-            "An error occurred while calling the Postgrest endpoint.",
-          status: response.status || 500,
-          errors: getErrorDetails(response.error),
-        };
-        throw supabaseError;
-      }
-      return response.data as T;
-    } catch (error) {
-      const supabaseError: SupabaseError = {
-        message:
-          error.message ||
-          "An error occurred while calling the Postgrest endpoint.",
-        status: error.status || 500,
-      };
-      throw supabaseError;
-    }
-  };
+//   const updateFieldPostgrest = async <T>(
+//     table: string,
+//     key: string,
+//     field: string,
+//     payload?: object
+//   ): Promise<T> => {
+//     try {
+//       const response: PostgrestResponse<T> = await supabase
+//         .from(table)
+//         .update(payload)
+//         .eq(field, key)
+//         .select()
+//         .single();
+//       if (response.error) {
+//         const supabaseError: SupabaseError = {
+//           message:
+//             response.error.message ||
+//             "An error occurred while calling the Postgrest endpoint.",
+//           status: response.status || 500,
+//           errors: getErrorDetails(response.error),
+//         };
+//         throw supabaseError;
+//       }
+//       return response.data as T;
+//     } catch (error) {
+//       const supabaseError: SupabaseError = {
+//         message:
+//           error.message ||
+//           "An error occurred while calling the Postgrest endpoint.",
+//         status: error.status || 500,
+//       };
+//       throw supabaseError;
+//     }
+//   };
 
-  const insertPostgrest = async <T>(
-    table: string,
-    payload?: object
-  ): Promise<T> => {
-    try {
-      const response: PostgrestResponse<T> = await supabase
-        .from(table)
-        .insert(payload)
-        .select()
-        .single();
-      if (response.error) {
-        const supabaseError: SupabaseError = {
-          message:
-            response.error.message ||
-            "An error occurred while calling the Postgrest endpoint.",
-          status: response.status || 500,
-          errors: getErrorDetails(response.error),
-        };
-        throw supabaseError;
-      }
-      return response.data as T;
-    } catch (error) {
-      const supabaseError: SupabaseError = {
-        message:
-          error.message ||
-          "An error occurred while calling the Postgrest endpoint.",
-        status: error.status || 500,
-      };
-      throw supabaseError;
-    }
-  };
+//   const insertPostgrest = async <T>(
+//     table: string,
+//     payload?: object
+//   ): Promise<T> => {
+//     try {
+//       const response: PostgrestResponse<T> = await supabase
+//         .from(table)
+//         .insert(payload)
+//         .select()
+//         .single();
+//       if (response.error) {
+//         const supabaseError: SupabaseError = {
+//           message:
+//             response.error.message ||
+//             "An error occurred while calling the Postgrest endpoint.",
+//           status: response.status || 500,
+//           errors: getErrorDetails(response.error),
+//         };
+//         throw supabaseError;
+//       }
+//       return response.data as T;
+//     } catch (error) {
+//       const supabaseError: SupabaseError = {
+//         message:
+//           error.message ||
+//           "An error occurred while calling the Postgrest endpoint.",
+//         status: error.status || 500,
+//       };
+//       throw supabaseError;
+//     }
+//   };
 
-  const getErrorDetails = (error: any): string[] | undefined => {
-    if (typeof error.details === "string") {
-      return [error.details];
-    }
-    if (Array.isArray(error.details)) {
-      return error.details;
-    }
-    return undefined;
-  };
+//   const getErrorDetails = (error: any): string[] | undefined => {
+//     if (typeof error.details === "string") {
+//       return [error.details];
+//     }
+//     if (Array.isArray(error.details)) {
+//       return error.details;
+//     }
+//     return undefined;
+//   };
 
-  return {
-    callEdgeFunction,
-    selectByFieldPostgrest,
-    updateFieldPostgrest,
-    insertPostgrest,
-  };
-};
+//   return {
+//     callEdgeFunction,
+//     selectByFieldPostgrest,
+//     updateFieldPostgrest,
+//     insertPostgrest,
+//   };
+// };
--- src/utils/middleware.tsx
+++ src/utils/middleware.tsx
@@ -1,24 +1,24 @@
-import { createMiddlewareSupabaseClient } from "@supabase/auth-helpers-nextjs";
-import { NextResponse } from "next/server";
-import type { NextRequest } from "next/server";
+// import { createMiddlewareSupabaseClient } from "@supabase/auth-helpers-nextjs";
+// import { NextResponse } from "next/server";
+// import type { NextRequest } from "next/server";
 
-export async function middleware(req: NextRequest) {
-  const res = NextResponse.next();
-  const supabase = createMiddlewareSupabaseClient({ req, res });
-  const {
-    data: { session },
-  } = await supabase.auth.getSession();
+// export async function middleware(req: NextRequest) {
+//   const res = NextResponse.next();
+//   const supabase = createMiddlewareSupabaseClient({ req, res });
+//   const {
+//     data: { session },
+//   } = await supabase.auth.getSession();
 
-  if (session) {
-    return res;
-  }
+//   if (session) {
+//     return res;
+//   }
 
-  const redirectUrl = req.nextUrl.clone();
-  redirectUrl.pathname = "/auth/signIn";
-  redirectUrl.searchParams.set(`redirectedFrom`, req.nextUrl.pathname);
-  return NextResponse.redirect(redirectUrl);
-}
+//   const redirectUrl = req.nextUrl.clone();
+//   redirectUrl.pathname = "/auth/signIn";
+//   redirectUrl.searchParams.set(`redirectedFrom`, req.nextUrl.pathname);
+//   return NextResponse.redirect(redirectUrl);
+// }
 
-export const config = {
-  matcher: "/client/:path*",
-};
+// export const config = {
+//   matcher: "/client/:path*",
+// };
